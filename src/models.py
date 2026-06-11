ff28318e00f5f71b6a5ac6e2369e720cfd643a02
Samuel Carmona <samuel.carmona.rodrigz@gmail.com>
2026-06-11 03:22:15 +0000

    Add database models and migration scripts for tournaments, subscriptions, predictions, and matches

--- src/models.py
+++ src/models.py
@@ -1,15 +1,18 @@
+from enum import Enum as pyEnum
+
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String, Boolean, Date, ForeignKey, Enum
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    email: Mapped[str] = mapped_column(
+        String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
-
 
     def serialize(self):
         return {
@@ -17,3 +20,58 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+
+class Torneo(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(90), unique=True, nullable=True)
+
+    sede: Mapped[str] = mapped_column(String(120), nullable=False)
+
+    fecha_inicio: Mapped[Date] = mapped_column(Date())
+    fecha_final: Mapped[Date] = mapped_column(Date())
+
+
+class Subscripciones(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    user: Mapped[User] = relationship()
+
+    torneo_id: Mapped[int] = mapped_column(ForeignKey("torneo.id"))
+    torneo: Mapped[Torneo] = relationship()
+
+
+class Resultado(pyEnum):
+    EMPATE = 0
+    GANA_LOCAL = 1
+    GANA_VISITANTE = 2
+
+
+class Partidos(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    equipo_local: Mapped[str] = mapped_column(String(120))
+    equipo_visitante: Mapped[str] = mapped_column(String(120))
+    fecha: Mapped[Date] = mapped_column(Date())
+    etapa: Mapped[str] = mapped_column(String(24))
+
+    resultado: Mapped[Resultado] = mapped_column(Enum(Resultado))
+
+    torneo_id: Mapped[int] = mapped_column(ForeignKey("torneo.id"))
+    torneo: Mapped[Torneo] = relationship()
+
+
+class Premios(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    nombre: Mapped[str] = mapped_column(String(120))
+    descripcion: Mapped[str] = mapped_column(String(360))
+
+
+class Prediccion(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+
+    user_prediction: Mapped[Resultado] = mapped_column(Enum(Resultado))
+
+    partido_id: Mapped[int] = mapped_column(ForeignKey("partidos.id"))
+    partido: Mapped[Partidos] = relationship()
